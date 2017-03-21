82613af8421246176d6809c204e5d8602fdececd
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-21 19:05:17 +0000

    Completed all features for adding character counter to new tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+function characterCounter() {
+  $( ".new-tweet textarea" ).keyup(function() {
+    let counterValue = 140 - $(this).val().length;
+    $(this).siblings(".counter").text(counterValue);
+    if (counterValue < 0) {
+      $(this).siblings(".counter").css("color", "red");
+    } else {
+      $(this).siblings(".counter").css("color", "black")
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', characterCounter);

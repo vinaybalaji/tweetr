196ac365f047341915b91946b4c6f04a3fe5885c
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-22 04:15:04 +0000

    Added minor changes to app.js file to hard code tweetObject and define variables for createTweetElement function.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ let tweetObject = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+function createTweetElement(tweetObject) {
+  let tweetName = tweetObject.user.name;
+  let tweetHandle = tweetObject.handle;
+  let tweetAvatar = tweetObject.user.avatars.regular;
+  let tweetBody = tweetObject.context.text;
+  let tweetAgeMilliseconds = tweetObject.created_at;
+
+}
+
+document.addEventListener('DOMContentLoaded', createTweetElement);

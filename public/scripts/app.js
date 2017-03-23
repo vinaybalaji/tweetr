ce70b6e00b350cb06ff7866260ac9e07b241934f
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-23 20:51:29 +0000

    Added minor changes to tweetAge function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -94,15 +94,14 @@ function createTweetElement(tweetData) {
 }
 
 function tweetAge(tweetAgeMilliseconds) {
-  var tweetAge = 0;
   if (Math.round((Date.now() - tweetAgeMilliseconds)/86400000) !== 0) {
     return Math.round((Date.now() - tweetAgeMilliseconds)/86400000) + " day(s) go";
-  } else if (Math.round((Date.now() - tweetAgeMilliseconds)/3600000) !== 0) {
-    return Math.round((Date.now() - tweetAgeMilliseconds)/3600000) + "hour(s) ago";
-  } else if (Math.round((Date.now() - tweetAgeMilliseconds)/60000) !==  0) {
-    return Math.round((Date.now() - tweetAgeMilliseconds)/60000) + " minute(s) ago";
+  } else if (Math.round((Date.now() - tweetAgeMilliseconds)/3600000) > 1) {
+    return Math.round((Date.now() - tweetAgeMilliseconds)/3600000) + "hours ago";
+  } else if (Math.round((Date.now() - tweetAgeMilliseconds)/3600000) ===  1) {
+    return "A few minutes ago";
   } else {
-    return Math.round((Date.now() - tweetAgeMilliseconds)/1000) + " second(s) ago";
+    return "A few seconds ago";
   }
 }
 
